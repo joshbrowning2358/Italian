86fcaddcda4ded5336f572556c22b5794d5194fc
rockclimber112358 <rockclimber112358@gmail.com>
2015-01-15 05:47:34 +0100

    New computeStats, updated Josh file, new Maria file!

--- computeStats.R
+++ computeStats.R
@@ -1,5 +1,6 @@
 computeStats <- function(){
     library(ggplot2)
+    library(scales)
     cat("Most successes:\n")
     cat(paste(vocabFile[which.max(vocabFile[,3]),]), "\n")
     cat("Most failures:\n")
@@ -7,8 +8,20 @@ computeStats <- function(){
     cat("Highest Success Rate:\n")
     cat(paste(vocabFile[which.max(vocabFile[,3]/vocabFile[,4]*(vocabFile[,4]>1)),]), "\n")
     cat("Lowest Success Rate:\n")
-    cat(paste(vocabFile[which.min(vocabFile[,3]/vocabFile[,4]*vocabFile[,4]>1),]), "\n")
-    qplot(vocabFile[,3]/vocabFile[,4], fill=vocabFile[,4]
+    if(min(vocabFile[, 3]) == 0){
+        temp = vocabFile[vocabFile[, 3] == 0, ]
+        cat(paste(temp[which.max(temp[, 4]), ]), "\n")
+    } else {
+        cat(paste(vocabFile[which.min(vocabFile[,3]/vocabFile[,4]*(vocabFile[,4]>1)),]), "\n")
+    }
+    print(qplot(vocabFile[,3]/vocabFile[,4], fill=vocabFile[,4]
             ,group=vocabFile[,4], binwidth=.05) +
-        labs(x="Success Rate")
+        labs(x = "Success Rate", y = "Number of Words", fill = "Total") +
+        scale_x_continuous(label=percent))
+    cat("\n")
+    if(min(vocabFile[, 3]) == 0){
+        cat("Words which have never been correctly answered:\n")
+        for(i in 1:nrow(temp))
+            cat(paste(temp[i, ]), "\n")
+    }
 }
